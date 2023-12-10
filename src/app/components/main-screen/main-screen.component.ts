9d2f8ce53e8d7901b18c26b7bbd40411426154ee
Ivan Tsarev <ivantsaryok@yandex.ru>
2023-12-10 23:26:54 +0400

    add main app screen

---
+++ src/app/components/main-screen/main-screen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-screen',
+  templateUrl: './main-screen.component.html',
+  styleUrl: './main-screen.component.scss'
+})
+export class MainScreenComponent {
+
+}
