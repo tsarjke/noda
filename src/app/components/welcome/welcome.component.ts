5bd8db5fefe8ce410d6963f999fb3b24d89211bc
Ivan Tsarev <ivantsaryok@yandex.ru>
2023-12-10 23:28:12 +0400

    add welcome section with form

---
+++ src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { selectOption } from '../../models/selectOption';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.scss'
+})
+export class WelcomeComponent {
+
+}
