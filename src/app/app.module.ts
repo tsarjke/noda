6697c578cc8ec4001613982d78978c60ec74c657
Ivan Tsarev <ivantsaryok@yandex.ru>
2023-12-10 23:31:29 +0400

    form page with all sections, add some common styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SwiperDirective } from './directives/swiper.directive';
+import { register } from 'swiper/element/bundle';
+import { FormsModule } from '@angular/forms';
+import { TextInputComponent } from '../core/modules/UI/text-input/text-input..component';
+import { CheckboxComponent } from '../core/modules/UI/checkbox/checkbox.component';
+import { SelectComponent } from '../core/modules/UI/select/select.component';
+import { ButtonComponent } from '../core/modules/UI/button/button.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MainScreenComponent } from './components/main-screen/main-screen.component';
+import { PreFooterComponent } from './components/pre-footer/pre-footer.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { WelcomeFormComponent } from './components/welcome/welcome-form/welcome-form.component';
+
+register();
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SwiperDirective,
+    TextInputComponent,
+    CheckboxComponent,
+    SelectComponent,
+    ButtonComponent,
+    WelcomeComponent,
+    MainScreenComponent,
+    PreFooterComponent,
+    FooterComponent,
+    WelcomeFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
